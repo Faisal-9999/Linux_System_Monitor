feb3ec87c406de45f10fc2a143f9a5480c873587
Faisal-9999 <mukhtarfaisal03@gmail.com>
2025-12-25 23:52:03 +0500

    inital phase of system data

--- src/system_data.rs
+++ src/system_data.rs
@@ -1,13 +1,37 @@
 //AFTER COMPLETING THIS WORK ON PER PROCESS CPU_USAGE in THE LOG PARSER FILE
 
-fn cpu_usage() -> f64 {
-    0.0
+use sysinfo::{System, Networks};
+use std::{thread, time::Duration};
+pub struct SystemData {
+    hardware : System,
+    network : Networks
 }
 
-fn ram_usage() -> f64 {
-    0.0
-}
+impl SystemData {
+    fn initialize() -> Self {
+        Self {
+            hardware : System::new_all(),
+            network : Networks::new_with_refreshed_list(),
+        }
+    }
 
-fn network_usage() -> f64 {
-    0.0
+    fn refresh(&mut self) {
+        self.hardware.refresh_cpu_all();
+        self.hardware.refresh_memory();
+        self.network.refresh(false);
+    }
+
+    fn cpu_usage(&self) -> f64 {
+        //NEED TO WRITE CODE FOR THIS too tired rn
+        //SAme for functions below but will change it later
+        0.0
+    }
+
+    fn ram_usage(&self) -> f64 {
+        0.0
+    }
+
+    fn network_usage(&self) -> f64 {
+        0.0
+    }
 }
