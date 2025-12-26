97e47a5844534a0e4a6a60ba96379e266a8c6694
Faisal-9999 <mukhtarfaisal03@gmail.com>
2025-12-26 18:07:41 +0500

    just added the function for calculating whole system cpu, ram_usage, network_usage, disk speed

--- src/system_data.rs
+++ src/system_data.rs
@@ -1,11 +1,22 @@
 //AFTER COMPLETING THIS WORK ON PER PROCESS CPU_USAGE in THE LOG PARSER FILE
 //AND THEN WORK ON GET_TABLE_DATA in database connectivity file
 
-use sysinfo::{System, Networks};
-use std::{thread, time::Duration};
-pub struct SystemData {
+use sysinfo::{System, Networks, Disks};
+use std::{iter, thread, time::Duration};
+
+struct SystemData {
     hardware : System,
-    network : Networks
+    network : Networks,
+    disks : Disks
+}
+
+struct Snapshot {
+    overall_cpu_used : f32,
+    overall_ram_used : u64,
+    overall_net_down_kb : f64,
+    overall_net_up_kb : f64,
+    overall_disk_read_mb : f64,
+    overall_disk_write_mb : f64,
 }
 
 impl SystemData {
@@ -13,26 +24,47 @@ impl SystemData {
         Self {
             hardware : System::new_all(),
             network : Networks::new_with_refreshed_list(),
+            disks : Disks::new_with_refreshed_list(),
         }
     }
 
-    fn refresh(&mut self) {
-        self.hardware.refresh_cpu_all();
-        self.hardware.refresh_memory();
+    fn system_usage_stream(&mut self) -> impl Iterator<Item = Snapshot> {
+        self.hardware.refresh_all();
         self.network.refresh(false);
-    }
 
-    fn cpu_usage(&self) -> f64 {
-        //NEED TO WRITE CODE FOR THIS too tired rn
-        //SAme for functions below but will change it later
-        0.0
-    }
 
-    fn ram_usage(&self) -> f64 {
-        0.0
-    }
+        iter::from_fn(move || {
+            thread::sleep(Duration::from_secs(1));
 
-    fn network_usage(&self) -> f64 {
-        0.0
+            self.hardware.refresh_cpu_all();
+            self.hardware.refresh_memory();
+            self.network.refresh(false);
+            self.disks.refresh(false);
+
+            let mut total_read_mb = 0.0;
+            let mut total_write_mb = 0.0;
+
+            for disk in self.disks.list() {
+                total_read_mb += disk.usage().read_bytes as f64 / 1024.0 / 1024.0;
+                total_write_mb += disk.usage().written_bytes as f64 / 1024.0 / 1024.0;
+            }
+
+            let mut down_speed = 0.0;
+            let mut up_speed = 0.0;
+
+            for (_name, data) in self.network.iter() {
+                down_speed += data.received() as f64 / 1024.0;
+                up_speed += data.transmitted() as f64 / 1024.0;
+            }
+
+            Some(Snapshot{
+                overall_cpu_used : self.hardware.global_cpu_usage(),
+                overall_ram_used : self.hardware.used_memory() / 1024 / 1024,
+                overall_net_down_kb : down_speed,
+                overall_net_up_kb : up_speed,
+                overall_disk_read_mb : total_read_mb,
+                overall_disk_write_mb : total_write_mb
+            })
+        })
     }
 }
